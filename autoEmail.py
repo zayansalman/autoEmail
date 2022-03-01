966665754ee6db60463ba45fa8e4642063624810
Zayan Salman <zayan_k@icloud.com>
2022-03-01 16:58:40 +0000

    DEV for each recipient

--- autoEmail.py
+++ autoEmail.py
@@ -1,27 +1,28 @@
 import smtplib
 from credentials import EMAIL, PASSWORD, MAIL_LIST
 
-sent_from = EMAIL #your email address
-to = MAIL_LIST #your password
-subject = 'Auto Email Test'
-body = 'Also test'
+for each in MAIL_LIST:
+    sent_from = EMAIL #your email address
+    to = each #your password
+    subject = 'Auto Email Test'
+    body = 'Also test'
 
-email_text = """\
-From: %s
-To: %s
-Subject:%s
+    email_text = """\
+    From: %s
+    To: %s
+    Subject:%s
+    
+    %s
+    """ % (sent_from, to, subject, body)
 
-%s
-""" % (sent_from, ",".join(to), subject, body)
-
-try:
-    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp_server.ehlo()
-    smtp_server.login(EMAIL, PASSWORD) #add your email address and password
-    smtp_server.sendmail(sent_from, to, email_text)
-    smtp_server.close()
-    print("Email sent successfully!")
-except Exception as ex:
-    print("Something went wrong...", ex)
+    try:
+        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp_server.ehlo()
+        smtp_server.login(EMAIL, PASSWORD) #add your email address and password
+        smtp_server.sendmail(sent_from, to, email_text)
+        smtp_server.close()
+        print("Email sent successfully!")
+    except Exception as ex:
+        print("Something went wrong...", ex)
 
 
